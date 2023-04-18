fab21f09233be1cfd18d5c049c8a3d306ef71bc9
pawelkozolub <pkozolub@gmail.com>
2023-04-18 23:11:13 +0200

    Add showOptions method

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -2,6 +2,14 @@ package pl.coderslab;
 
 public class TaskManager {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        showOptions();
+    }
+
+    public static void showOptions() {
+        System.out.println(ConsoleColors.BLUE + "Please select an option:" + ConsoleColors.RESET);
+        System.out.println("add");
+        System.out.println("remove");
+        System.out.println("list");
+        System.out.println("exit");
     }
 }
